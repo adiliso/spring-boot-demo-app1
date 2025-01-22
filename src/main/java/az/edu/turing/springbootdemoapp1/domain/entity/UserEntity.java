b355e5b24be579b95c3213b727fc45f950c7a790
anarouis <anarftliyev@gmail.com>
2025-01-22 19:56:03 +0400

    jpa repository

--- src/main/java/az/edu/turing/springbootdemoapp1/domain/entity/UserEntity.java
+++ src/main/java/az/edu/turing/springbootdemoapp1/domain/entity/UserEntity.java
@@ -1,19 +1,27 @@
 package az.edu.turing.springbootdemoapp1.domain.entity;
 
 import az.edu.turing.springbootdemoapp1.model.enums.UserStatus;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class UserEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "username", nullable = false)
     private String username;
+
+    @ToString.Exclude
     private String password;
+
+    @Column(name = "status")
     private UserStatus userStatus;
 }
